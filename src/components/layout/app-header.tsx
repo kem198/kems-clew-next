508600b8f2f5da51ac809c13f2358bcbb00097f4
KeM198 <kenkenpa198@gmail.com>
2026-07-19 12:41:24 +0900

    feat: replace hardcoded site name with constant for improved maintainability

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -1,3 +1,4 @@
+import { SITE_NAME } from "@/constants/site";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
@@ -31,7 +32,7 @@ export function AppHeader({ className, ...props }: AppHeaderProps) {
           width={24}
           height={24}
         />
-        KeM&apos;s Clew
+        {SITE_NAME}
       </Link>
     </header>
   );
