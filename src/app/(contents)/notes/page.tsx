8b5997f283159579711243476ca3abe3f5c822c6
KeM198 <kenkenpa198@gmail.com>
2026-07-19 14:31:50 +0900

    fix: enhance layout styling for improved readability and consistency

--- src/app/(contents)/notes/page.tsx
+++ src/app/(contents)/notes/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { Tags } from "@/components/shared/tags";
+import { ubuntuSans } from "@/constants/fonts";
 import { formatDateToYYYYMMDD } from "@/lib/date";
 import { withSiteName } from "@/lib/seo";
 import { getNotes } from "@/utils/server/notes.server";
@@ -13,21 +13,36 @@ export default async function NotesPage() {
   const notes = await (await getNotes()).reverse();
 
   return (
-    <article className="prose">
-      <h1>Notes</h1>
+    <article className="w-full rounded-md bg-white p-6">
+      <div className="prose">
+        <h1>Notes</h1>
 
-      <ul className="not-prose flex flex-col gap-2 bg-red-200">
-        {notes.map((note) => (
-          <li key={note.slug} className="bg-blue-200">
-            <Link href={`/notes/${note.slug}`}>
-              {formatDateToYYYYMMDD(note.frontmatter.date)}
-              {note.frontmatter.title}
-              {/* // TODO: 記事の本分プレビューを表示する */}
-              {<Tags tags={note.frontmatter.tags}></Tags>}
-            </Link>
-          </li>
-        ))}
-      </ul>
+        <ul className="not-prose flex flex-col gap-12">
+          {notes.map((note) => (
+            <li key={note.slug} className="bg-blue-20 flex flex-col gap-1">
+              <span className={`${ubuntuSans.className} text-sm text-gray-400`}>
+                {formatDateToYYYYMMDD(note.frontmatter.date)}
+              </span>
+
+              <div className="flex flex-col gap-0">
+                <Link href={`/notes/${note.slug}`}>
+                  <span className="text-lg font-bold hover:underline">
+                    {note.frontmatter.title}
+                  </span>
+                </Link>
+
+                {/* // TODO: 記事の本分プレビューを表示する */}
+                {/* <p className="text-sm text-gray-400">
+                  あああああああああああああああああああああああああああああああああああaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaあ
+                </p> */}
+              </div>
+
+              {/* TODO: タグで絞り込み可能にする */}
+              {/* {<Tags tags={note.frontmatter.tags}></Tags>} */}
+            </li>
+          ))}
+        </ul>
+      </div>
     </article>
   );
 }
