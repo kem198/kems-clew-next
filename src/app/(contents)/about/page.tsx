413bcb1812b34d6a38d8418c5aac0a3413ef85e9
KeM198 <kenkenpa198@gmail.com>
2026-07-19 22:25:55 +0900

    feat: create About page with dynamic content from Markdown file

--- src/app/(contents)/about/page.tsx
+++ src/app/(contents)/about/page.tsx
@@ -1,15 +1,44 @@
-import { ContentArea } from "@/components/shared/content-area";
-import { withSiteName } from "@/lib/seo";
+import ContentArea from "@/components/shared/content-area";
+import { ubuntuSans } from "@/constants/fonts";
+import { formatDateToYYYYMMDD } from "@/lib/date";
+import { NoteFrontmatter } from "@/types/note";
+import { evaluate } from "next-mdx-remote-client/rsc";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
 
-export const metadata = {
-  title: withSiteName("About"),
-};
+export default async function Page() {
+  const filePath = path.join(process.cwd(), "contents/about/about.md");
 
-export default async function AboutPage() {
+  const source = await readFile(filePath, "utf-8");
+
+  const { frontmatter, content } = (await evaluate({
+    source,
+    options: {
+      parseFrontmatter: true,
+    },
+  })) as {
+    frontmatter: NoteFrontmatter;
+    content: React.ReactNode;
+  };
+
   return (
-    <ContentArea full>
-      <h1>About</h1>
-      <p>test</p>
+    <ContentArea className="prose">
+      <ul className="not-prose text-right text-gray-400">
+        <li>
+          作成日:{" "}
+          <span className={`${ubuntuSans.className}`}>
+            {formatDateToYYYYMMDD(frontmatter.date)}
+          </span>
+        </li>
+        <li>
+          更新日:{" "}
+          <span className={`${ubuntuSans.className}`}>
+            {formatDateToYYYYMMDD(frontmatter.lastmod)}
+          </span>
+        </li>
+      </ul>
+      <h1>{frontmatter?.title}</h1>
+      {content}
     </ContentArea>
   );
 }
