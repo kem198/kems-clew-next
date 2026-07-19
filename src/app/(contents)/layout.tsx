8e7e7eae9e2ee72fa5953afb4c99325f95f398de
KeM198 <kenkenpa198@gmail.com>
2026-07-20 04:24:32 +0900

    optimize breadcrumbs step3

--- src/app/(contents)/layout.tsx
+++ src/app/(contents)/layout.tsx
@@ -1,14 +1,11 @@
-import { Breadcrumbs } from "@/components/shared/breadcrumbs";
-
 export default function ContentLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div className="w-full">
       <div className="mx-auto flex w-full max-w-6xl flex-col gap-4 px-6 py-4 max-md:p-3">
-        <Breadcrumbs segments={[]} />
         {children}
       </div>
     </div>
