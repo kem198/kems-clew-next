33fdeade25273b06ad99ca85dc33725d2c77baa5
KeM198 <kenkenpa198@gmail.com>
2026-07-19 11:56:52 +0900

    refactor: simplify AppHeader component by moving link rendering inside the component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { AppHeader } from "@/components/layout/app-header";
 import { notoSansJp } from "@/constants/fonts";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -25,17 +23,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen">
         <div className="flex min-h-screen flex-col">
-          <AppHeader>
-            <Link href="/" className="flex gap-2">
-              <Image
-                src="/icons/icon-192x192.png"
-                alt="icon"
-                width={24}
-                height={24}
-              />
-              KeM&apos;s Clew
-            </Link>
-          </AppHeader>
+          <AppHeader />
           <main className="flex-1 p-4">{children}</main>
           <AppFooter />
         </div>
--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -1,20 +1,38 @@
-"use client";
-
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 import * as React from "react";
 
 export type AppHeaderProps = React.HTMLAttributes<HTMLElement>;
 
-export function AppHeader({ className, children, ...props }: AppHeaderProps) {
+export function AppHeader({ className, ...props }: AppHeaderProps) {
+  // TODO: 現在パスが HOME のときは色々表示する
+  // const pathname = usePathname();
+  // const resolvedIsHome = pathname === "/";
+
   return (
     <header
       className={cn(
-        "flex h-12 w-full items-center gap-2 bg-gray-800 p-2 text-white",
+        `h-12 w-full items-center gap-2 bg-gray-800 p-2 font-normal text-white`,
         className,
       )}
       {...props}
     >
-      {children}
+      <Link
+        href="/"
+        className={cn(
+          "flex gap-2",
+          // resolvedIsHome && "text-4xl"
+        )}
+      >
+        <Image
+          src="/icons/icon-192x192.png"
+          alt="icon"
+          width={24}
+          height={24}
+        />
+        KeM&apos;s Clew
+      </Link>
     </header>
   );
 }
