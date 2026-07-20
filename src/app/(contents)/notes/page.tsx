0318bdb8b626b60954e69d34a9e6b9b0832ca77a
KeM198 <kenkenpa198@gmail.com>
2026-07-21 03:30:43 +0900

    Add tag counts display to NotesPage component

--- src/app/(contents)/notes/page.tsx
+++ src/app/(contents)/notes/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { Breadcrumbs } from "@/components/shared/breadcrumbs";
 import { ContentArea } from "@/components/shared/content-area";
-import { Tags } from "@/components/shared/note-tag";
+import { Tag, Tags } from "@/components/shared/note-tag";
 import { BreadcrumbSegment } from "@/constants/breadcrumbs";
 import { ubuntuSans } from "@/constants/fonts";
 import { formatDateToYYYYMMDD } from "@/lib/date";
@@ -16,11 +16,35 @@ export const metadata = {
 export default async function NotesPage() {
   const notes = await getSortedNotes("desc");
 
+  const tagCounts = new Map<string, number>();
+
+  for (const note of notes) {
+    for (const tag of note.frontmatter.tags ?? []) {
+      tagCounts.set(tag, (tagCounts.get(tag) ?? 0) + 1);
+    }
+  }
+
+  const tags = Array.from(tagCounts.entries()).sort(([a], [b]) =>
+    a.localeCompare(b),
+  );
+
   return (
     <>
       <Breadcrumbs segments={[BreadcrumbSegment.notes]} />
       <ContentArea>
         <h1>Notes</h1>
+
+        <div className="not-prose flex flex-col gap-2">
+          <h2 className="text-lg font-bold">Tags</h2>
+
+          <ul className="flex flex-wrap gap-2 text-cyan-600">
+            {tags.map(([tag, count]) => (
+              <li key={tag}>
+                <Tag tag={tag} /> ({count})
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <ul className="not-prose flex flex-col gap-12">
           {notes.map((note) => (
