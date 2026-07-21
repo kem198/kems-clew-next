27e5100b694e61354f477079351758ff52089287
KeM198 <kenkenpa198@gmail.com>
2026-07-21 22:39:49 +0900

    Add RSS feed generation and update site constants for improved metadata

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,1 +1,5 @@
 export const SITE_NAME = "KeM's Clew";
+export const SITE_DESCRIPTION = "@KeM198 のホームページ。";
+export const SITE_URL = "https://clew.kem198.net";
+
+export const RSS_TITLE = `${SITE_NAME} RSS Feed`;
