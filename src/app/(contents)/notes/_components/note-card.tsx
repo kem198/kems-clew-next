c8d15d221d1190bb2fa7c6f4ecc75bec136ff512
KeM198 <kenkenpa198@gmail.com>
2026-07-21 21:56:27 +0900

    Refactor NoteCard component layout for improved structure and readability

--- src/app/(contents)/notes/_components/note-card.tsx
+++ src/app/(contents)/notes/_components/note-card.tsx
@@ -1,6 +1,7 @@
 import { TagBadgeList } from "@/app/(contents)/notes/_components/note-tag";
 import { ubuntuSans } from "@/constants/fonts";
 import { formatDateToYYYYMMDD } from "@/lib/date";
+import { cn } from "@/lib/utils";
 import type { Note } from "@/types/note";
 import Link from "next/link";
 
@@ -11,22 +12,20 @@ export type NoteCardProps = {
 
 export function NoteCard({ note, className }: NoteCardProps) {
   return (
-    <article className={className}>
+    <article className={cn("flex flex-col gap-1", className)}>
       <span className={`${ubuntuSans.className} text-sm text-zinc-400`}>
         {formatDateToYYYYMMDD(note.frontmatter.date)}
       </span>
 
-      <div className="flex flex-col gap-1">
-        <Link href={`/notes/${note.slug}`}>
-          <span className="text-primary/90 text-xl font-bold hover:underline">
-            {note.frontmatter.title}
-          </span>
-        </Link>
+      <Link href={`/notes/${note.slug}`}>
+        <span className="text-primary/90 text-xl font-bold hover:underline">
+          {note.frontmatter.title}
+        </span>
+      </Link>
 
-        {note.preview ? (
-          <p className="text-sm text-zinc-400">{note.preview}</p>
-        ) : null}
-      </div>
+      {note.preview ? (
+        <p className="text-sm text-zinc-400">{note.preview}</p>
+      ) : null}
 
       <TagBadgeList tags={note.frontmatter.tags} />
     </article>
