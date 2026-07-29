61cacdc6b7191b04b353d1d08c7eeba1f72f8f73
KeM198 <kenkenpa198@gmail.com>
2026-07-29 22:00:37 +0900

    Refactor font imports and variables for improved consistency in layout and styles

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -2,16 +2,20 @@ import { Geist, Geist_Mono, Noto_Sans_JP, Ubuntu_Sans } from "next/font/google";
 
 export const geistSans = Geist({
   subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 export const geistMono = Geist_Mono({
   subsets: ["latin"],
-});
-
-export const ubuntuSans = Ubuntu_Sans({
-  subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const notoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+});
+
+export const ubuntuSans = Ubuntu_Sans({
+  subsets: ["latin"],
+  variable: "--font-ubuntu-sans",
 });
